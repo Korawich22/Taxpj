6293fa49e4602db29245c7446cc552bddf529800
Korawich22 <6531503002@lamduan.mfu.ac.th>
2024-04-08 13:31:05 +0700

    Add net income

--- taxcal/lib/singcal.dart
+++ taxcal/lib/singcal.dart
@@ -32,7 +32,8 @@ class Singcal extends StatefulWidget {
 }
 
 class _SingcalState extends State<Singcal> {
-  double calculatedTax = 0.0; // Initialize with 0
+  double totaldeducttion = 0.0; // Initialize with 0
+  double taxcaculation = 0.0;
   bool calculationError = false;
 
   @override
@@ -55,7 +56,7 @@ class _SingcalState extends State<Singcal> {
 
       // Perform your tax calculation here based on the received data
       // This is just an example calculation, replace it with your actual calculation logic
-      double totalDeductions = lifeHealthInsurance +
+      double deductions = lifeHealthInsurance +
           healthInsuranceParent +
           homeLoan +
           socialSecurity +
@@ -64,9 +65,26 @@ class _SingcalState extends State<Singcal> {
           (widget.isDisFatherSelected ? 60000 : 0) +
           (widget.isDisMotherSelected ? 60000 : 0) +
           (widget.isOtherSelected ? 60000 : 0);
-
-      calculatedTax = income - totalDeductions; // Example calculation
+      totaldeducttion = (income - deductions - 160000); // Example calculation
       calculationError = false;
+
+      if (totaldeducttion >= 5000001) {
+        taxcaculation = ((totaldeducttion - 5000000) * 0.35) + 126500;
+      } else if (totaldeducttion >= 2000001 && totaldeducttion <= 5000000) {
+        taxcaculation = ((totaldeducttion - 2000000) * 0.3) + 365000;
+      } else if (totaldeducttion >= 1000001 && totaldeducttion <= 2000000) {
+        taxcaculation = ((totaldeducttion - 1000000) * 0.25) + 115000;
+      } else if (totaldeducttion >= 750001 && totaldeducttion <= 1000000) {
+        taxcaculation = ((totaldeducttion - 750000) * 0.2) + 65000;
+      } else if (totaldeducttion >= 500001 && totaldeducttion <= 750000) {
+        taxcaculation = ((totaldeducttion - 500000) * 0.15) + 27500;
+      } else if (totaldeducttion >= 300001 && totaldeducttion <= 500000) {
+        taxcaculation = ((totaldeducttion - 300000) * 0.1) + 7500;
+      } else if (totaldeducttion >= 150001 && totaldeducttion <= 300000) {
+        taxcaculation = ((totaldeducttion - 150000) * 0.05) + 0;
+      } else {
+        taxcaculation = 0;
+      }
     } catch (e) {
       print("Error calculating tax: $e");
       setState(() {
@@ -78,145 +96,171 @@ class _SingcalState extends State<Singcal> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color(0xFF0C3A2D),
-        title: const Text(
-          'Back',
-          style: TextStyle(
-            fontFamily: 'Poppins',
-            color: Colors.white,
+        appBar: AppBar(
+          backgroundColor: const Color(0xFF0C3A2D),
+          title: const Text(
+            'Back',
+            style: TextStyle(
+              fontFamily: 'Poppins',
+              color: Colors.white,
+            ),
           ),
-        ),
-        leading: GestureDetector(
-          onTap: () {
-            Navigator.pop(context);
-          },
-          child: Transform.scale(
-            scale: 0.8,
-            child: Container(
-              padding: const EdgeInsets.all(8),
-              decoration: const BoxDecoration(
-                shape: BoxShape.circle,
-                color: Color(0xFFFFB902),
-              ),
-              child: const Icon(
-                Icons.arrow_back,
-                color: Color(0xFF0C3A2D),
+          leading: GestureDetector(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Transform.scale(
+              scale: 0.8,
+              child: Container(
+                padding: const EdgeInsets.all(8),
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Color(0xFFFFB902),
+                ),
+                child: const Icon(
+                  Icons.arrow_back,
+                  color: Color(0xFF0C3A2D),
+                ),
               ),
             ),
           ),
         ),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
               const SizedBox(height: 20),
               Column(
                 children: [
                   const SizedBox(height: 20),
-                  Text(
-                    calculationError
-                        ? 'Error calculating tax'
-                        : 'TAXES TO BE PAID: \$${calculatedTax.toStringAsFixed(2)}', // Display calculated tax
-                    style: TextStyle(
-                      fontFamily: 'Poppins',
-                      fontSize: 20,
-                      color: calculationError
-                          ? Colors.red
-                          : const Color(0xFF998E8E),
-                    ),
-                  ),
-                  const SizedBox(height: 20),
                   Container(
                     width: 250,
-                    height: 250,
-                    decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Color(0xFFFFB902),
+                    height: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.grey[
+                          200], // Change the color according to your preference
                     ),
-                    child: const Center(
-                      child: Text(
-                        '\$0', // Placeholder text for tax amount
+                    child: Center(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20.0), // Add horizontal padding
+                        child: Text(
+                          'YOUR NET INCOME : ${totaldeducttion >= 0 ? totaldeducttion.toStringAsFixed(2) : '0.00'}', // Display the totaldeducttion value
+                          style: const TextStyle(
+                            fontFamily: 'Poppins',
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      const SizedBox(height: 20),
+                      Text(
+                        calculationError
+                            ? 'Error calculating tax'
+                            : 'TAXES TO BE PAID', // Display calculated tax, ensuring it's not negative
                         style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
+                          fontFamily: 'Poppins',
+                          fontSize: 20,
+                          color: calculationError
+                              ? Colors.red
+                              : const Color(0xFF998E8E),
                         ),
                       ),
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  const Text(
-                    '** This tax calculator is considered a preliminary calculation only by using data from tax year 2023. Our application does not guarantee the accuracy of the data and results. For additional deductions in tax year 2023, please check further from the announcement of the Revenue Department.',
-                    style: TextStyle(
-                      fontFamily: 'Poppins',
-                      fontSize: 12,
-                      color: Colors.grey,
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const FirstPage(),
+                      const SizedBox(height: 20),
+                      Container(
+                        width: 250,
+                        height: 250,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Color(0xFFFFB902),
                         ),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xFF6D9674),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(14),
-                      ),
-                      minimumSize: const Size(double.infinity, 60),
-                    ),
-                    child: const Text(
-                      'GOT IT!',
-                      style: TextStyle(
-                        fontFamily: 'Poppins',
-                        fontWeight: FontWeight.bold,
-                        fontSize: 14,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const FirstPage(),
+                        child: Center(
+                          child: Text(
+                            '\฿${taxcaculation >= 0 ? taxcaculation.toStringAsFixed(2) : '0.00'}', // Placeholder text for tax amount
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
                         ),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xFF6D9674),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(14),
                       ),
-                      minimumSize: const Size(double.infinity, 60),
-                    ),
-                    child: const Text(
-                      'FIND PLACE',
-                      style: TextStyle(
-                        fontFamily: 'Poppins',
-                        fontWeight: FontWeight.bold,
-                        fontSize: 14,
-                        color: Colors.white,
+                      const SizedBox(height: 20),
+                      const Text(
+                        '** This tax calculator is considered a preliminary calculation only by using data from tax year 2023. Our application does not guarantee the accuracy of the data and results. For additional deductions in tax year 2023, please check further from the announcement of the Revenue Department.',
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontSize: 12,
+                          color: Colors.grey,
+                        ),
                       ),
-                    ),
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const FirstPage(),
+                            ),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFF6D9674),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(14),
+                          ),
+                          minimumSize: const Size(double.infinity, 60),
+                        ),
+                        child: const Text(
+                          'GOT IT!',
+                          style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const FirstPage(),
+                            ),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFF6D9674),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(14),
+                          ),
+                          minimumSize: const Size(double.infinity, 60),
+                        ),
+                        child: const Text(
+                          'FIND PLACE',
+                          style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
+            ]),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
